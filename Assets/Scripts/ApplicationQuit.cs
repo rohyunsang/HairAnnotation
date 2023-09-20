2d34987a99c98e3af6c60788c6feee79d99673b6
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-09-20 16:06:16 +0900

    Init Project

--- Assets/Scripts/ApplicationQuit.cs
+++ Assets/Scripts/ApplicationQuit.cs
@@ -5,14 +5,12 @@ using UnityEngine.UI;
 
 public class ApplicationQuit : MonoBehaviour
 {
-    public Text saveText;
-    public Text copySaveText;
+    
     public GameObject checkQuitImage;
 
     public void IsQuitApplication()
     {
         checkQuitImage.SetActive(true);
-        copySaveText.text = saveText.text + " \n" + "정말로 종료하시겠습니까? ";
     }
     
     public void OffCheckQuitImage()
--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -8,123 +8,25 @@ public class PanelManager : MonoBehaviour
 {
     public GameObject InitPanel;
     public GameObject OptionPanel;
-    public GameObject SavePanel;
-    public GameObject LoginPanel;
-    public GameObject StatusPanel;
+    public GameObject MainPanel;
 
-
-    public GameObject faceField;
-
-    public GameObject ImageClickInfo;
-    public GameObject ImageEditInfo;
-
-    public GameObject SaveInfo;
-    public GameObject SaveCompleteInfo;
-    public GameObject pimpleSaveCompleteInfo;
-
-    public Text uncheckingStatusText;
-
-    public void OnPimpleSaveCompleteInfo()
-    {
-        pimpleSaveCompleteInfo.SetActive(true);
-    }
-    public void OffPimpleSaveCompleteInfo()
-    {
-        pimpleSaveCompleteInfo.SetActive(false);
-    }
-
-    public void OffSaveInfo()
-    {
-        SaveInfo.SetActive(false);
-        uncheckingStatusText.text = "\n 피부 상태를 체크해주세요. \n";
-    }
-
-    public void OnSaveInfo()
-    {
-        SaveInfo.SetActive(true);
-    }
-
-    public void OnSaveCompleteInfo()
-    {
-        SaveCompleteInfo.SetActive(true);
-    }
-    
-    public void OffSaveCompleteInfo()
-    {
-        SaveCompleteInfo.SetActive(false);
-    }
-
-    public void OnImageEditInfo()
-    {
-        ImageEditInfo.SetActive(true);
-    }
-
-    public void OffImageEditInfo()
-    {
-        ImageEditInfo.SetActive(false);
-    }
-
-    public void OnStatusPanel()
-    {
-        StatusPanel.SetActive(true);
-    }
-    public void OffStatusPanel()
-    {
-        StatusPanel.SetActive(false);
-    }
-
-    public void OffImageClickInfo()
-    {
-        Invoke("InvokeOffImageClickInfo", 3f);
-    }
-
-    public void InvokeOffImageClickInfo()
-    {
-        ImageClickInfo.SetActive(false);
-    }
-    public void OnInitPanel()
-    {
+    public void OnInitPanel(){
         InitPanel.SetActive(true);
     }
-
-    public void OnOptionPanel()
-    {
-        OptionPanel.SetActive(true);
-    }
-    public void OnSavePanel()
-    {
-        SavePanel.SetActive(true);
-    }
-
-    public void OnLoginPanel()
-    {
-        LoginPanel.SetActive(true);
-    }
-    public void OffLoginPanel()
-    {
-        LoginPanel.SetActive(false);
-    }
-    public void OffSavePanel()
-    {
-        SavePanel.SetActive(false);
-    }
-
-    public void OffInitPanel()
-    {
+    public void OffInitPanel(){
         InitPanel.SetActive(false);
     }
-
-    public void OffOptionPanel()
-    {
+    public void OnOptionPanel(){
+        OptionPanel.SetActive(true);
+    }
+    public void OffOptionPanel(){
         OptionPanel.SetActive(false);
     }
-    
-    public void DeleteObjects()
-    {
-        foreach (Transform child in faceField.transform)
-        {
-            Destroy(child.gameObject);
-        }
-    }
 
+    public void OnMainPanel(){
+        MainPanel.SetActive(true);
+    }
+    public void OffMainPanel(){
+        MainPanel.SetActive(false);
+    }
 }
